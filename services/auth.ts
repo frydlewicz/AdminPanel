348d2e94c0977434f48cf1ceff03874de6298ea7
Kamil Frydlewicz <kamil@frydlewicz.pl>
2021-03-27 10:48:42 +0100

    users credentials

--- services/auth.ts
+++ services/auth.ts
@@ -1,14 +1,11 @@
 import { NextPageContext } from 'next';
 import basicAuthMiddleware from 'nextjs-basic-auth-middleware';
 
+import config from '../config.json';
+
 export async function authMiddleware(ctx: NextPageContext) {
     await basicAuthMiddleware(ctx.req, ctx.res, {
-        users: [
-            {
-                name: 'admin',
-                password: 'admin',
-            },
-        ],
+        users: config.users,
     });
 
     return {};
