d2f67116a6226c8be9fcd16c7b1ea0bd071feb8b
Kamil Frydlewicz <kamil@frydlewicz.pl>
2022-12-23 00:29:39 +0100

    redis host and port

--- services/redis.ts
+++ services/redis.ts
@@ -1,8 +1,12 @@
 import redis from 'redis';
 
+import config from '../config.json';
 import { IObject, Resolver, Rejecter } from '../helpers/types';
 
-const client = redis.createClient();
+const client = redis.createClient({
+    host: config.redis?.host || 'localhost',
+    port: config.redis?.port || 6379,
+});
 
 export function getData(key: string): Promise<IObject> {
     return new Promise((res: Resolver<IObject>, rej: Rejecter): void => {
